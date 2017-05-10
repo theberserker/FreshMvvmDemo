6fe3234672cf82d1818a44effc81a88cf7af5491
Andrej Spilak <aspilak@pixi.eu>
2017-05-10 17:11:43 +0200

    remove dialog on detail

--- FreshMvvmDemo/FreshMvvmDemo/FreshMvvmDemo/PageModels/ParticipantPageModel.cs
+++ FreshMvvmDemo/FreshMvvmDemo/FreshMvvmDemo/PageModels/ParticipantPageModel.cs
@@ -20,14 +20,5 @@ namespace FreshMvvmDemo.PageModels
             Participant = initData as Participant;
 
         }
-
-
-        protected override void ViewIsAppearing(object sender, EventArgs e)
-        {
-            base.ViewIsAppearing(sender, e);
-
-            string message = string.Format("Entramos al detalle del contacto {0}", Participant.Name);
-            CoreMethods.DisplayAlert("Detalle", message, "Ok");
-        }
     }
 }
